b5ba18edc1c6dac5a080c1a0ad8f4cba3ef30835
Nikita Samusev <nikitf777@gmail.com>
2026-01-31 18:56:07 +0300

    Prevent selecting the same file twice

--- wgpaper-daemon-actix-web/src/handlers.rs
+++ wgpaper-daemon-actix-web/src/handlers.rs
@@ -1,25 +1,22 @@
-use actix_web::{HttpResponse, Responder, web};
+use std::sync::Mutex;
+
+use actix_web::{
+	HttpResponse, Responder,
+	web::{self},
+};
 use calloop::channel::Sender;
 use lib_wgpaper_daemon::app::Commands;
 use wgpaper_config::Config;
 
-use crate::random_file::select_random_file;
+use crate::services::TransitionService;
 
 pub async fn start_transition(
 	sender: web::Data<Sender<Commands>>,
 	config: web::Data<Config>,
+	transition_service: web::Data<Mutex<TransitionService>>,
 ) -> impl Responder {
-	let path = select_random_file(
-		config.wallpaper_directories().unwrap_or_default(),
-		&[".jpg", ".png"],
-		&[] as &[&str],
-	)
-	.unwrap();
-	let command = Commands::StartTransition {
-		image_path: path.to_str().unwrap().to_string(),
-	};
-	sender
-		.send(command)
-		.map(|_| HttpResponse::Ok().body("OK"))
+	let mut service = transition_service.lock().unwrap();
+	service
+		.start_transition(sender, config)
 		.unwrap_or_else(|_| HttpResponse::ServiceUnavailable().body("SCTK offline"))
 }
