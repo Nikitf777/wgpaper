e7a820265a11ee38442ea29419c8a2b342886591
Nikita Samusev <nikitf777@gmail.com>
2026-02-15 15:33:47 +0300

    Remove unused code in wgpu_texture.rs

--- lib-wgpaper-daemon/src/renderer/wgpu/wgpu_texture.rs
+++ lib-wgpaper-daemon/src/renderer/wgpu/wgpu_texture.rs
@@ -1,6 +1,3 @@
-use anyhow::*;
-use image::GenericImageView;
-
 pub struct WgpuTexture {
 	#[allow(unused)]
 	pub texture: wgpu::Texture,
@@ -8,78 +5,6 @@ pub struct WgpuTexture {
 }
 
 impl WgpuTexture {
-	pub fn from_bytes(
-		device: &wgpu::Device,
-		queue: &wgpu::Queue,
-		bytes: &[u8],
-		label: &str,
-	) -> Result<Self> {
-		let img = image::load_from_memory(bytes)?;
-		Self::from_image(device, queue, &img, Some(label))
-	}
-
-	pub fn from_image(
-		device: &wgpu::Device,
-		queue: &wgpu::Queue,
-		img: &image::DynamicImage,
-		label: Option<&str>,
-	) -> Result<Self> {
-		let rgba = img.to_rgba8();
-		let dimensions = img.dimensions();
-
-		let size = wgpu::Extent3d {
-			width: dimensions.0,
-			height: dimensions.1,
-			depth_or_array_layers: 1,
-		};
-		let format = wgpu::TextureFormat::Rgba8UnormSrgb;
-		let texture = device.create_texture(&wgpu::TextureDescriptor {
-			label,
-			size,
-			mip_level_count: 1,
-			sample_count: 1,
-			dimension: wgpu::TextureDimension::D2,
-			format,
-			usage: wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_DST,
-			view_formats: &[],
-		});
-
-		queue.write_texture(
-			wgpu::TexelCopyTextureInfo {
-				aspect: wgpu::TextureAspect::All,
-				texture: &texture,
-				mip_level: 0,
-				origin: wgpu::Origin3d::ZERO,
-			},
-			&rgba,
-			wgpu::TexelCopyBufferLayout {
-				offset: 0,
-				bytes_per_row: Some(4 * dimensions.0),
-				rows_per_image: Some(dimensions.1),
-			},
-			size,
-		);
-
-		let view = texture.create_view(&wgpu::TextureViewDescriptor::default());
-
-		Ok(Self { texture, view })
-	}
-
-	pub fn from_bytes_with_format(
-		device: &wgpu::Device,
-		queue: &wgpu::Queue,
-		bytes: &[u8],
-		label: &str,
-		format: wgpu::TextureFormat,
-	) -> anyhow::Result<Self> {
-		// Decode image (using image crate)
-		let img = image::load_from_memory(bytes)?;
-		let rgba = img.into_rgba8();
-		let dimensions = rgba.dimensions();
-
-		Self::from_rgba8_with_format(device, queue, dimensions, &rgba, label, format)
-	}
-
 	pub fn from_rgba8_with_format(
 		device: &wgpu::Device,
 		queue: &wgpu::Queue,
